465e687fef6eb2f2a08b198c5f6aed990ec8afcd
_ <_@_>
2022-06-05 21:11:27 +0800

    feat: render frame

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,31 @@
+import { blocks } from './constants/blockMap'
 import './index.css'
 import { getGrid } from './utils/getGrid'
+import { getRandomBlock } from './utils/getRandomBlock'
 
 const config = { gridSize: { row: 20, col: 10 }, speed: 1000 }
+const getInitialState = () => ({
+  solidGrid: getGrid(config.gridSize),
+  currentGrid: getGrid(config.gridSize),
+  displayGrid: getGrid(config.gridSize),
+  clock: undefined as number | undefined,
+  stopped: true,
+  score: 0,
+  currentBlock: {
+    x: 6,
+    y: 0,
+    blockCode: getRandomBlock(),
+  },
+})
 
-let solidGrid = getGrid(config.gridSize)
-let currentGrid = getGrid(config.gridSize)
-let displayGrid = getGrid(config.gridSize)
-let clock: number
-let stopped = true
+let state = getInitialState()
 
-const tick = () => {
-  console.log('tick')
-}
-
-const updateStoppedState = () => {
-  const stop = document.querySelector('#stop')?.classList
-  if (!stop) return
-  if (stopped) {
-    clearInterval(clock)
-    stop.add('active')
-  } else {
-    clock = setInterval(tick, config.speed)
-    stop.remove('active')
-  }
-}
-
-window.onload = () => {
+const draw = (grid: (string | number)[][]) => {
   const playArea = document.querySelector('#grid')
   if (!playArea) return
+  playArea.innerHTML = ''
 
-  displayGrid.forEach((row, rowIdx) => {
+  grid.forEach((row, rowIdx) => {
     const rowEle = document.createElement('div')
     rowEle.className = 'grid-row'
     rowEle.id = 'R' + rowIdx
@@ -45,14 +41,66 @@ window.onload = () => {
 
     playArea.appendChild(rowEle)
   })
+}
 
+const updateGrid = () => {
+  const newCurrentGrid = getGrid(config.gridSize)
+  const block = blocks[state.currentBlock.blockCode]
+
+  block.forEach((row, rowIdx) => {
+    if (state.currentBlock.y - rowIdx >= 0) {
+      row.forEach((value, colIdx) => {
+        if (value) {
+          newCurrentGrid[state.currentBlock.y - rowIdx][
+            state.currentBlock.x - colIdx
+          ] = state.currentBlock.blockCode
+        }
+      })
+    }
+  })
+
+  const newDisplayGrid = getGrid(config.gridSize).map((row, rowIdx) =>
+    row.map(
+      (col, colIdx) =>
+        state.currentGrid[rowIdx][colIdx] || state.solidGrid[rowIdx][colIdx]
+    )
+  )
+
+  state.currentGrid = newCurrentGrid
+  state.displayGrid = newDisplayGrid
+
+  draw(newDisplayGrid)
+}
+
+const tick = () => {
+  console.log('tick')
+  state.score = state.score + 1
+  state.currentBlock.y += 1
+  updateGrid()
+}
+
+const updateStoppedState = () => {
+  const stop = document.querySelector('#stop')?.classList
+  if (!stop) return
+  if (state.stopped) {
+    clearInterval(state.clock)
+    state.clock = undefined
+    stop.add('active')
+  } else {
+    state.clock = setInterval(tick, config.speed)
+    stop.remove('active')
+  }
+}
+
+window.onload = () => {
+  updateGrid()
   updateStoppedState()
 }
 
 document.addEventListener('keyup', (e) => {
   switch (e.key.toUpperCase()) {
     case 'S':
-      stopped = !stopped
+      state.stopped = !state.stopped
       updateStoppedState()
       break
   }
--- src/utils/getGrid.ts
+++ src/utils/getGrid.ts
@@ -1,2 +1,8 @@
-export const getGrid = ({ row, col }: { row: number; col: number }): 0[][] =>
-  new Array(row).fill(new Array(col).fill(0))
+export const getGrid = ({
+  row,
+  col,
+}: {
+  row: number
+  col: number
+}): (string | number)[][] =>
+  new Array(row).fill(0).map(() => new Array(col).fill(0))
